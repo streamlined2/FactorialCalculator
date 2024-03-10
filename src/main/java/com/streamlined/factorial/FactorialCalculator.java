7a723c624bf952fef73c4f52973913e5d86fc664
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-03-10 23:28:28 +0200

    Stealing thread pool used to split and join computation task

--- src/main/java/com/streamlined/factorial/FactorialCalculator.java
+++ src/main/java/com/streamlined/factorial/FactorialCalculator.java
@@ -6,21 +6,37 @@ import java.util.concurrent.RecursiveTask;
 public class FactorialCalculator {
 
 	public BigInteger compute(int number) {
-		return new FactorialTask(number).compute();
+		return new FactorialTask(2, number).compute();
 	}
 
 	private static class FactorialTask extends RecursiveTask<BigInteger> {
 
-		private final int number;
+		private static final int RANGE_LENGTH = 10;
 
-		private FactorialTask(int number) {
-			this.number = number;
+		private final int start;
+		private final int finish;
+
+		private FactorialTask(int start, int finish) {
+			this.start = start;
+			this.finish = finish;
 		}
 
 		@Override
 		protected BigInteger compute() {
+			if (finish - start + 1 <= RANGE_LENGTH) {
+				return calculate();
+			} else {
+				final int middle = (start + finish) / 2;
+				FactorialTask task1 = new FactorialTask(start, middle);
+				FactorialTask task2 = new FactorialTask(middle + 1, finish);
+				invokeAll(task1, task2);
+				return task1.join().multiply(task2.join());
+			}
+		}
+
+		private BigInteger calculate() {
 			BigInteger value = BigInteger.ONE;
-			for (int k = 2; k <= number; k++) {
+			for (int k = start; k <= finish; k++) {
 				value = value.multiply(BigInteger.valueOf(k));
 			}
 			return value;
